fad6227e20de8af646e1873a58c30bb50b5eff8b
unknown <luis.moraes37@etec.sp.gov.br>
2019-05-26 11:57:19 -0300

    exe2

---
+++ src/course2/Principal.java
@@ -0,0 +1,30 @@
+package course2;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String sentence = sc.nextLine();
+		String x, y, z;
+		x =sc.next();
+		y = sc.next();
+		z = sc.next();
+		
+		System.out.println(sentence);
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		
+		x =sc.next();
+		y = sc.next();
+		z = sc.next();
+		
+		sc.close();
+
+	}
+
+}
