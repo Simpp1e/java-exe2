4aebe1ca4e91c083bbd9b95d017cb18128e017eb
Simpp1e <luis.moraes37@etec.sp.gov.br>
2019-05-26 12:29:31 -0300

    types entrance

--- src/course2/Principal.java
+++ src/course2/Principal.java
@@ -1,28 +1,40 @@
 package course2;
 
+import java.util.Locale;
+
 import java.util.Scanner;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
 		
-		String sentence = sc.nextLine();
-		String x, y, z;
-		x =sc.next();
-		y = sc.next();
-		z = sc.next();
+		int n1 = sc.nextInt();
+		sc.nextLine();
+		String name = sc.nextLine();
+		char gender = sc.next().charAt(0);
 		
-		System.out.println(sentence);
-		System.out.println(x);
-		System.out.println(y);
-		System.out.println(z);
+		String s = sc.next();
+		char letter = s.charAt(0);
+		int digit = Integer.parseInt (s.substring(1));
+		double n2 = sc.nextDouble();
+		String name2 = sc.next();
+		char ch = sc.next().charAt(0);
+		int age = sc.nextInt();
+		double height = sc.nextDouble();
 		
-		x =sc.next();
-		y = sc.next();
-		z = sc.next();
-		
+		System.out.println(n1);
+		System.out.println(name);
+		System.out.println(gender);
+		System.out.println(letter);
+		System.out.println(digit);
+		System.out.println(n2);
+		System.out.println(name2);
+		System.out.println(ch);
+		System.out.println(age);
+		System.out.println(height);
 		sc.close();
 
 	}
